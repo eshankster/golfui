e18c4f5c0af1f3d6c39f2f86703895e233604394
Friel, Frank <frank.friel@fmr.com>
2020-03-18 19:13:45 +0000

    Adding additional files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PlayerSelect from './components/PlayerSelect';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="headertext">
+          PGA Player Statistics (2010 to 2018)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="paratext">
+          Select a player and then a statistic variable to see results (where available) from 2010 to 2018.
+        </p>
+        <PlayerSelect />
       </header>
     </div>
   );
---
+++ src/components/PlayerSelect.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import ReactHtmlParser from 'react-html-parser';
+import Spinner from 'react-spinner-material';
+import ReactDOM from 'react-dom';
+import axios from "axios";
+
+export class PlayerSelect extends Component {
+  constructor() {
+    super();
+    this.state = {
+      player: '0',
+      variable: '0',
+    };
+    this.onChangePlayer = this.onChangePlayer.bind(this);
+    this.onChangeVariable = this.onChangeVariable.bind(this);
+    this.submitSearch = this.submitSearch.bind(this);
+  }
+  render() {
+    return (
+      <div className="container">
+       <div className="input-field col s12">
+          <select onChange={this.onChangePlayer} className="dropdown">
+            <option disabled selected>Select a player</option>
+            <option>Adam Scott</option>
+            <option>Bubba Watson</option>
+            <option>Brooks Koepka</option>
+            <option>Charl Schwartzel</option>
+            <option>Dustin Johnson</option>
+            <option>Edoardo Molinari</option>
+            <option>Graeme McDowell</option>
+            <option>Henrik Stenson</option>
+            <option>Ian Poulter</option>
+            <option>J.B. Holmes</option>
+            <option>Jason Day</option>
+            <option>Jim Furyk</option>
+            <option>Jordan Spieth</option>
+            <option>Justin Rose</option>
+            <option>Lee Westwood</option>
+            <option>Martin Kaymer</option>
+            <option>Matt Kuchar</option>
+            <option>Padraig Harrington</option>
+            <option>Phil Mickelson</option>
+            <option>Rickie Fowler</option>
+            <option>Rory McIlroy</option>
+            <option>Sergio Garcia</option>
+            <option>Shane Lowry</option>
+            <option>Tiger Woods</option>
+            <option>Tommy Fleetwood</option>
+          </select>
+        </div>
+        <div className="input-field col s12">
+          <select onChange={this.onChangeVariable} className="dropdown">
+            <option disabled selected>Select a variable</option>
+            <option>Carry Distance - (AVG.)</option>
+            <option>Driving Distance - (AVG.)</option>
+            <option>FedExCup Standings - (POINTS)</option>
+            <option>Greens in Regulation Percentage - (%)</option>
+            <option>Hit Fairway Percentage - (%)</option>
+            <option>Longest Drives - (DISTANCE)</option>
+            <option>Official Money - (YTD VICTORIES)</option>
+            <option>Official World Golf Ranking - (TOTAL POINTS)</option>
+            <option>Putting Average - (AVG)</option>
+            <option>Scoring Average (Actual) - (AVG)</option>
+        </select>
+      </div>
+      <div className="input-field col s12">
+        <button onClick={this.submitSearch} className="buttoncls">Submit</button>
+      </div>
+      <div className="input-field col s12" id="result"></div>
+    </div>
+    );
+  }
+
+  onChangePlayer(e) {
+    this.setState({ player: e.target.value });
+  }
+
+  onChangeVariable(e) {
+    this.setState({ variable: e.target.value });
+  }
+
+  submitSearch() {
+    if (this.state.player !== '0' && this.state.variable !== '0') {
+      let element = <div className="spinnerdiv"><Spinner radius={50} color={"#000"} stroke={4} visible={this.state.visible} /></div>
+      ReactDOM.render(element, document.getElementById('result'));
+      console.log('submit');
+      axios.get('http://localhost:3001/search?player=' + this.state.player + '&variable=' + this.state.variable)
+      .then(function (response) {
+        console.log(response);
+        // this.changeVisibility(false);
+        // console.log('a', JSON.parse(response.data));
+        let bufferOriginal = Buffer.from(response.data[0].data);
+        ReactDOM.render(<div className="actresult">{ ReactHtmlParser(bufferOriginal.toString().replace(/\n/g, "<br />")) }</div>, document.getElementById('result'));
+      })
+      .catch(function (error) {
+        // this.changeVisibility(false);
+        console.log(error);
+      });
+    }
+  }
+}
+
+export default PlayerSelect;
